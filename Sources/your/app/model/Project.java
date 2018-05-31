0e86c7402a826e157d594643025802275cc0ce4f
domschoen <dschoenenberger@mac.com>
2018-05-31 08:20:55 +0200

    ERDList with calculated attributes

--- Sources/your/app/model/Project.java
+++ Sources/your/app/model/Project.java
@@ -3,7 +3,16 @@ package your.app.model;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.webobjects.eoaccess.EOUtilities;
+import com.webobjects.foundation.NSArray;
+
 public class Project extends _Project {
 	@SuppressWarnings("unused")
 	private static final Logger log = LoggerFactory.getLogger(Project.class);
+	
+	
+	// Let's have a calculated relationship
+	public NSArray<Customer> potentialCustomers() {
+		return (NSArray<Customer>)EOUtilities.objectsForEntityNamed(editingContext(),Customer.ENTITY_NAME);
+	}
 }
